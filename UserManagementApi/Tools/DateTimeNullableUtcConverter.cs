327aa2a69015b0861e76cc27a004bdbebac44d14
Nikolai Marchenko <kolymarchenko2004@gmail.com>
2025-06-16 13:41:19 +0300

    feat(Tools): add dateTime converter

---
+++ UserManagementApi/Tools/DateTimeNullableUtcConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace UserManagementApi.Tools
+{
+    public class DateTimeNullableUtcConverter : JsonConverter<DateTime?>
+    {
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            => reader.TokenType == JsonTokenType.Null
+                ? null
+                : DateTime.SpecifyKind(reader.GetDateTime(), DateTimeKind.Utc);
+
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+        {
+            if (value.HasValue)
+                writer.WriteStringValue(value.Value.ToUniversalTime());
+            else
+                writer.WriteNullValue();
+        }
+    }
+}
---
+++ UserManagementApi/Tools/DateTimeUtcConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace UserManagementApi.Tools
+{
+    public class DateTimeUtcConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            => DateTime.SpecifyKind(reader.GetDateTime(), DateTimeKind.Utc);
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+            => writer.WriteStringValue(value.ToUniversalTime());
+    }
+}
